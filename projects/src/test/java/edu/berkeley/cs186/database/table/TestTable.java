43937ba99aef6194f12ce2f1ece3f16ac66ce84a
William Smith <williampsmith@berkeley.edu>
2017-02-14 17:56:02 -0800

    final submission

--- projects/src/test/java/edu/berkeley/cs186/database/table/TestTable.java
+++ projects/src/test/java/edu/berkeley/cs186/database/table/TestTable.java
@@ -553,13 +553,13 @@ public class TestTable {
   @Test(expected = NoSuchElementException.class)
   @Category(StudentTest.class)
   public void testEmptyPageNullIterator() throws DatabaseException {
-    Iterator<Record> nullIter = table.iterator();
+    Iterator<Record> nullIterator = table.iterator();
     Record newRecord = TestUtils.createRecordWithAllTypes();
 
     //create one empty page
     RecordID newRecordID = table.addRecord(newRecord.getValues());
     newRecord = table.deleteRecord(newRecordID);
 
-    Record failedRecord = nullIter.next();
+    Record failedRecord = nullIterator.next();
   }
 }
